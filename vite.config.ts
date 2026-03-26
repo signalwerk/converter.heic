ec95e14deef41a229dc1f6f19db515c0b4fcf5ff
signalwerk <sh@signalwerk.ch>
2026-03-26 12:35:40 +0100

    FIX: base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repository = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const base =
-  process.env.GITHUB_ACTIONS === 'true' && repository && !repository.endsWith('.github.io')
-    ? `/${repository}/`
-    : '/'
-
 export default defineConfig({
-  base,
+  base: './',
   plugins: [react()],
 })
